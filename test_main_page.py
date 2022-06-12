8bd80508fc639eb1d035177eb34710e61bec99b5
vaskina <katyvem@gmail.com>
2022-06-12 14:08:34 +0300

    преобразование теста test_main_page.py с помощью Page Object

--- test_main_page.py
+++ test_main_page.py
@@ -1,3 +1,4 @@
+from .pages.main_page import MainPage
 import pytest
 from selenium import webdriver
 from selenium.webdriver.common.by import By
@@ -8,9 +9,15 @@ from selenium.webdriver.common.by import By
 #    login_link = browser.find_element(By.CSS_SELECTOR, "#login_link")
 #    login_link.click()
 
-def go_to_login_page(browser):
-    login_link = browser.find_element(By.CSS_SELECTOR, "#login_link")
-    login_link.click()
-def test_guest_can_go_to_login_page(browser): 
-   browser.get(link) 
-   go_to_login_page(browser)
+##def go_to_login_page(browser):
+##    login_link = browser.find_element(By.CSS_SELECTOR, "#login_link")
+##    login_link.click()
+##def test_guest_can_go_to_login_page(browser): 
+##   browser.get(link) 
+##   go_to_login_page(browser)
+
+def test_guest_can_go_to_login_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес 
+    page.open()                      # открываем страницу
+    page.go_to_login_page()          # выполняем метод страницы — переходим на страницу логина
